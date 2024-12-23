cd90292d37f108bc8e63f2546ec235fdf0792db2
andhuc <anhduc2337@gmail.com>
2024-12-24 00:34:54 +0700

    Implement infinite scrolling and search functionality on the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import SidebarLayout from "../components/SidebarLayout";
-import { Button, Col, Row, Select } from "antd";
+import { Button, Col, Input, Row, Select } from "antd";
 import ItemCard from "../components/item/ItemCard";
 import URLMapping, { API_URL } from "../utils/URLMapping";
 import BaseService from "../services/BaseService";
 import { useLoading } from "../hooks/useLoading";
+import { set } from "lodash";
+import Search, { SearchProps } from "antd/es/input/Search";
 
 const Home: React.FC = () => {
   const { logout } = useAuth();
@@ -14,6 +16,16 @@ const Home: React.FC = () => {
   const [items, setItems] = React.useState<any>([]);
   const { showLoading, hideLoading } = useLoading();
   const [categories, setCategories] = React.useState<any>([]);
+  const [totalPage, setTotalPage] = React.useState(1);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [search, setSearch] = React.useState<string>("");
+  const [selectedCategory, setSelectedCategory] = React.useState<
+    number | string
+  >("0");
+
+  useEffect(() => {
+    loadData();
+  }, [currentPage, selectedCategory]);
 
   useEffect(() => {
     (async () => {
@@ -25,14 +37,47 @@ const Home: React.FC = () => {
       setCategories(categoryData?.data ?? []);
       hideLoading();
     })();
-    loadData();
+    setCurrentPage(1);
   }, []);
 
   const loadData = async () => {
+    if (currentPage > totalPage) return;
     showLoading();
-    const itemData = await BaseService.get(URLMapping.GET_ITEMS_HOME, false);
-    setItems(itemData?.data?.queryable ?? []);
+    const queryParams = `page=${currentPage}&search=${search}&categoryId=${
+      selectedCategory !== 0 ? selectedCategory : ""
+    }`;
+    const itemData = await BaseService.get(
+      `${URLMapping.GET_ITEMS_HOME}?${queryParams}`,
+      false
+    );
+    setItems((prevItems: any) => [
+      ...prevItems,
+      ...(itemData?.data?.queryable ?? []),
+    ]);
+    setTotalPage(itemData?.data?.pageCount ?? 1);
     hideLoading();
+  };
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    const bottom =
+      (e.target as HTMLElement).scrollHeight ===
+      (e.target as HTMLElement).scrollTop +
+        (e.target as HTMLElement).clientHeight;
+    if (bottom && currentPage < totalPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handleSearch: SearchProps["onSearch"] = (value, _e, info) => {
+    setCurrentPage(1);
+    setItems([]);
+    loadData();
+  };
+
+  const handleCategoryChange = (value: number | string) => {
+    setSelectedCategory(value);
+    setCurrentPage(1);
+    setItems([]);
   };
 
   return (
@@ -41,9 +86,20 @@ const Home: React.FC = () => {
         <Col>
           <h1>Home</h1>
         </Col>
-        <Col>
-          <Select defaultValue="all" style={{ width: 120 }}>
-            <Select.Option key="all" value="all">
+        <Col style={{ display: 'flex' }}>
+          <Search style={{ marginRight: "1rem" }}
+            placeholder="Input search text"
+            onSearch={handleSearch}
+            enterButton
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <Select
+            style={{ width: 120 }}
+            onChange={handleCategoryChange}
+            value={selectedCategory}
+          >
+            <Select.Option key="0" value="0">
               All
             </Select.Option>
             {categories.map((category: any) => (
@@ -55,7 +111,11 @@ const Home: React.FC = () => {
         </Col>
       </Row>
 
-      <Row gutter={[32, 32]} style={{ paddingTop: "2rem" }}>
+      <Row
+        gutter={[32, 32]}
+        style={{ paddingTop: "2rem" }}
+        onScroll={handleScroll}
+      >
         {items.map((item: any) => (
           <Col xs={24} sm={12} md={6} lg={5} key={item.id}>
             <ItemCard
